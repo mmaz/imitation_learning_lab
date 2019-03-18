c041620ecadb2c1e85abfb25b8bc2aeb2e9186c9
Mark Mazumder <mark@markmaz.com>
2019-03-18 16:48:34 -0400

    RACECAR recording script

---
+++ record_RACECAR.py
@@ -0,0 +1,73 @@
+from __future__ import print_function
+import rospy
+import cv2 as cv
+import numpy as np
+from ackermann_msgs.msg import AckermannDriveStamped
+import os
+import datetime
+
+ngl = None
+speed = None
+
+# dev/video*
+LEFT   = 2
+CENTER = 3
+RIGHT  = 1
+
+def joycb(msg):
+    global ngl
+    global speed
+    ngl   = msg.drive.steering_angle
+    speed = msg.drive.speed
+
+def main():
+    global ngl
+    global speed
+    rospy.init_node('record')
+    JOY_TOPIC = "/vesc/low_level/ackermann_cmd_mux/input/teleop"
+
+    rospy.Subscriber(JOY_TOPIC, AckermannDriveStamped, joycb)
+    
+    cap_l = cv.VideoCapture(LEFT)
+    #cap_l.set(cv.CAP_PROP_FPS, 15)
+    cap_l.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+    cap_l.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+    
+    cap_c = cv.VideoCapture(CENTER)
+    #cap_c.set(cv.CAP_PROP_FPS, 15)
+    cap_c.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+    cap_c.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+    
+    cap_r = cv.VideoCapture(RIGHT)
+    #cap_r.set(cv.CAP_PROP_FPS, 15)
+    cap_r.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+    cap_r.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+
+    start = datetime.datetime.now()
+    
+    data_folder = "run_{}".format(start.strftime("%m_%d_%H_%M"))
+    os.mkdir(data_folder)
+    os.chdir(data_folder)
+
+    csv_fn = "{}.csv".format(data_folder)
+
+    while True and (ngl is not None and speed is not None):
+        ret_l, frame_l = cap_l.read()
+        ret_c, frame_m = cap_c.read()
+        ret_r, frame_r = cap_r.read()
+        frames = np.hstack((frame_l, frame_m, frame_r))
+
+        now = datetime.datetime.now()
+        img_fn = "{}.jpg".format(now.strftime('%m_%d_%H_%M_%S_%f')[:-3])
+        #cv.imwrite(img_fn, frames, [int(cv.IMWRITE_PNG_COMPRESSION), 5])
+        cv.imwrite(img_fn, frames, [int(cv.IMWRITE_JPEG_QUALITY), 85])
+        with open(csv_fn, 'a') as fh:
+            fh.write("{},{},{}\n".format(img_fn, ngl, speed))
+        if rospy.is_shutdown():
+            cap_l.release()
+            cap_c.release()
+            cap_r.release()
+            cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
