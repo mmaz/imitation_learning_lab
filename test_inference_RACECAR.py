f8056df2f94e5a56ef0f86359b3a12b801f7e248
Mark Mazumder <mark@markmaz.com>
2019-11-18 13:54:27 -0500

    upgrade tf

--- test_inference_RACECAR.py
+++ test_inference_RACECAR.py
@@ -13,7 +13,9 @@ config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.25 
 sess = tf.InteractiveSession(config=config)
 
-MODEL='pilotnet-RACECAR-001.h5'
+#MODEL='pilotnet-RACECAR-001.h5'
+#MODEL='tensorflow2.0.0.h5'
+MODEL='tf_115_p3.h5'
 model = load_model(MODEL)
 
 model._make_predict_function() # http://projectsfromtech.blogspot.com/2017/10/visual-object-recognition-in-ros-using.html
@@ -23,7 +25,9 @@ for ix in range(103):
     # goofy wait for warmup
     if ix == 3:
         start = datetime.datetime.now()
-    random_img = np.random.rand(1,66,200,3)
+    #random_img = np.random.rand(1,66,200,3)
+    random_img = np.random.rand(120,280,3)
+    random_img = np.expand_dims(random_img, 0)
     with graph.as_default():
         ngl = model.predict(random_img, batch_size=1)[0,0]
     print("model result:", ngl)
